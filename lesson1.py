5b1009570b170df377495d354e85c0b28a6fc6b9
sreekantha26 <srikanth.nrsa@gmail.com>
2024-09-27 15:04:44 +1000

    added lesson3

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+print ("Hello World")
+
+Tickets_price = 80
+
+if Tickets_price > 80 :
+    print("Price greater than 80")
+else:
+    print("Price less than 80")
+
+#____________________________________________________________
+
+height = int(input("what is your height\n"))
+if height >= 120 :
+    print("you can ride")
+else:
+    print("you can't ride")
+
+#____________________________________________________________
+
+integer = int(input("Input number"))
+
+if integer%2 == 0:
+    print("even")
+else:
+    print("odd")
+
