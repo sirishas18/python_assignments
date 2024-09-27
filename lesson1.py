d73326c2bc9d6aaa3f836cb29236fec4cd984c51
sreekantha26 <srikanth.nrsa@gmail.com>
2024-09-27 18:51:07 +1000

    adding tasks

--- lesson1.py
+++ lesson1.py
@@ -15,6 +15,7 @@ if height >= 120 :
 else:
     print("you can't ride")
 
+
 #____________________________________________________________
 
 integer = int(input("Input number"))
@@ -23,4 +24,30 @@ if integer%2 == 0:
     print("even")
 else:
     print("odd")
+
+#--------------------
+
+bill = 0
+height = int(input("what is your height\n"))
+if height >= 120 :
+    print("you can ride")
+    age = int(input("what is your age\n"))
+    if age <= 12:
+        bill = 5
+        print("Plz pay $5-child")
+    elif age <= 18:
+        bill = 7
+        print("Plz pay $7-youth")
+    else:
+        bill = 12
+        print("Plz pay $12")
+
+    want_photo = input("do you want to take PIC? y/n\n")
+    if want_photo == "y":
+     bill = bill+5
+
+    print(f"Your total cost is {bill}")
+
+else:
+    print("you can't ride")
 
---
+++ ls-3.1.py
@@ -0,0 +1,55 @@
+cost = 0
+bill = 0
+print("Order your Pizza")
+size = input("What size of Pizza do you want? s, m or l: \n ")
+if size == "s":
+    cost = 15
+    print("Plz pay $15")
+    Pepperoni = input("Do you want to add pepperoni? y or n : \n")
+    if Pepperoni == "y":
+        bill = cost + 2
+    else:
+        bill = cost
+        print(f"Your total cost is {bill}")
+    Cheese = input("Do you want extra cheese? y or n : ")
+    if Cheese == "y":
+        bill = bill + 1
+    else :
+        bill = bill
+    print(f"Your total cost is {bill}")
+
+elif size == "m":
+    cost = 20
+    print("Plz pay $20")
+    Pepperoni = input("Do you want to add pepperoni? y or n : \n")
+    if Pepperoni == "y":
+        bill = cost + 2
+    else:
+        bill = cost
+        print(f"Your total cost is {bill}")
+    Cheese = input("Do you want extra cheese? y or n : ")
+    if Cheese == "y":
+        bill = bill + 1
+    else :
+        bill = bill
+    print(f"Your total cost is {bill}")
+
+elif size == "l":
+    cost = 25
+    print("Plz pay $25")
+    Pepperoni = input("Do you want to add pepperoni? y or n : \n")
+    if Pepperoni == "y":
+        bill = cost + 3
+    else:
+        bill = cost
+        print(f"Your total cost is {bill}")
+    Cheese = input("Do you want extra cheese? y or n : ")
+    if Cheese == "y":
+        bill = bill + 1
+    else :
+        bill = bill
+    print(f"Your total cost is {bill}")
+
+else:
+    print("You have select wrong option.Plz select s or m or l")
+
