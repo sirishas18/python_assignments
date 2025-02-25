b6ba0c98f35d39fffc34a31c2d5dbf2048567aa2
sreekantha26 <srikanth.nrsa@gmail.com>
2025-02-25 11:30:46 +1100

    added a file

---
+++ Hang-man.py
@@ -0,0 +1,50 @@
+import random
+word_list = ["daisey", "hibiscus", "rose"]
+
+# Todo-1: Randomly choose a word and assign it to choosen word and print it
+# Todo-2: Ask user to guess word and assign it to variable guess_lowercase
+# Todo-3: Check guessed letter is from the choosen word and print right or wrong
+# Corrections to steps 1,2,3 follows
+# Todo-4: Create a placeholder with same no of blanks as chosen_word
+# Todo-5: Create a "display" that puts guess letter in right position and "-" in rest
+# Todo-6: use a while loop to guess another letter again
+# Todo-7: Change the for loop to keep previous correct and letter in display
+# Todo-8: create variable "lives"-to keep track of
+# Todo-9: any guess letter is not in choosen letter than reduce "lives" by 1
+# Todo-10: Print the ASCII art farm stages - corresponding to lives
+
+Choosen_word = random.choice(word_list)
+print(Choosen_word)
+
+placeholder = ""
+word_length = len(Choosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+game_over = False
+
+correct_letter = []
+
+while not game_over:
+    guess = input("Guess a letter from the word? : \n ").lower()
+
+    display = ""
+    for letter in Choosen_word:
+        if letter == guess :
+            display += letter
+            correct_letter.append(letter)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You guessed the right word.")
+
+
+
+
--- passwordgenerator.py
+++ passwordgenerator.py
@@ -2,12 +2,13 @@ import numbers
 import random
 import string
 
-from Tools.scripts.nm2def import symbols
+
 
 letter_l = list(string.ascii_lowercase)
 letter_u = list(string.ascii_uppercase)
 letters = letter_l + letter_u
 print(letters)
+#print(string.ascii_letters)
 #string.digits
 numbers = ('0','1','2','3','4','5','6','7','8','9')
 #string.punctuation
@@ -22,16 +23,15 @@ password = ""
 for char in range(1,nr_letters+1):
     password += random.choice(letters)
     #print(password)
-for num in range(1,nr_numbers+1):
+for char in range(1,nr_numbers+1):
     password += random.choice(numbers)
     #print(password)
 for char in range(1,nr_symbols+1):
     password += random.choice(symbol)
     #print(password)
-
 print(password)
 
-
+random.shuffle(password)
 
 
 
